04b01052fcbdf70b86e5b67893b2d57ef06b8adf
Xu Sun <sunxuhit@gmail.com>
2017-05-22 16:51:48 +0800

    v1.09: modify submit macro

--- FillSpinAlignment/submit/FillSpinAlignmentZdcSmd.sh
+++ FillSpinAlignment/submit/FillSpinAlignmentZdcSmd.sh
@@ -15,7 +15,7 @@ if [ $# -eq 0 ]
     # for((counter=0;counter<3;counter=counter+1)) # 19GeV
     # for((counter=0;counter<5;counter=counter+1)) # 27GeV
     # for((counter=0;counter<7;counter=counter+1)) # 62GeV
-    for((counter=0;counter<17;counter=counter+1)) # 200GeV
+    for((counter=0;counter<33;counter=counter+1)) # 200GeV
     do
       cp ./run.csh ./run$Name$counter.csh
 
