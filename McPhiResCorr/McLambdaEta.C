8bbf106bcaede900cb7fb1ee1d5ccff8ffdb55e6
Xu Sun <sunxuhit@gmail.com>
2017-06-14 22:35:52 +0800

    v1.5: add MC simulation for Lambdas

---
+++ McPhiResCorr/McLambdaEta.C
@@ -0,0 +1,392 @@
+#include <iostream>
+#include <string> 
+#include <map>
+#include "TFile.h"
+#include "TLorentzVector.h"
+#include "TPythia6.h"
+#include "TPythia6Decayer.h"
+#include "TParticle.h"
+#include "TRandom3.h"
+#include "TVector3.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TH3F.h"
+#include "TF1.h"
+#include "TStopwatch.h"
+#include "TMath.h"
+#include "TClonesArray.h"
+#include "TCanvas.h"
+#include "TGraphAsymmErrors.h"
+#include "TVector3.h"
+#include "TProfile.h"
+#include "/global/homes/x/xusun/STAR/VecMesonSpinAlignment/Utility/functions.h"
+#include "/global/homes/x/xusun/STAR/VecMesonSpinAlignment/Utility/StSpinAlignmentCons.h"
+
+using namespace std;
+
+float readRes(int energy, int pid, int centrality);
+TF1* readv2(int energy, int pid, int centrality);
+TF1* readspec(int energy, int pid, int centrality);
+TH1F* readeta(int energy, int pid, int centrality);
+void getKinematics(TLorentzVector& lPhi, double const mass);
+void setDecayChannels(int const pid);
+void decayAndFill(int const kf, TLorentzVector* lPhi, TClonesArray& daughters);
+void fill(TLorentzVector* lPhi, TLorentzVector const& lKplus, TLorentzVector const& lKminus);
+void write(int energy);
+TVector3 CalBoostedVector(TLorentzVector const lMcDau, TLorentzVector *lMcVec);
+float getChi(float Resolution);
+float EventPlaneSmearing(TF1 *f_EP);
+double calDipAngle(TLorentzVector const& lKplus, TLorentzVector const& lKminus);
+bool passDipAngleCut(TLorentzVector const& lKplus, TLorentzVector const& lKminus);
+bool passEtaCut(float eta, int BinEta);
+
+// histograms
+TH3F *h_Tracks;
+TProfile *p_cosRP;
+TProfile *p_CosEtaKaon[20], *p_CosEtaPhi[20];
+
+// sampling functions
+TF1 *f_v2, *f_spec, *f_flow, *f_EP;
+TH1F *h_eta;
+
+TPythia6Decayer* pydecay;
+
+void McLambdaEta(int energy = 6, int pid = 1, int cent = 0, int const NMax = 100000)
+{
+  int   const BinPt    = vmsa::BinPt;
+  int   const BinY     = vmsa::BinY;
+  int   const BinPhi   = vmsa::BinPhi;
+
+  h_Tracks = new TH3F("h_Tracks","h_Tracks",BinPt,vmsa::ptMin,vmsa::ptMax,10.0*BinY,-10.0,10.0,BinPhi,-TMath::Pi(),TMath::Pi());
+
+  p_cosRP = new TProfile("p_cosRP","p_cosRP",BinPt,vmsa::ptMin,vmsa::ptMax);
+
+  for(int i_eta = 0; i_eta < 20; ++i_eta)
+  {
+    string HistName;
+    HistName = Form("p_CosEtaKaon_%d",i_eta);
+    p_CosEtaKaon[i_eta] = new TProfile(HistName.c_str(),HistName.c_str(),BinPt,vmsa::ptMin,vmsa::ptMax);
+    HistName = Form("p_CosEtaPhi_%d",i_eta);
+    p_CosEtaPhi[i_eta] = new TProfile(HistName.c_str(),HistName.c_str(),BinPt,vmsa::ptMin,vmsa::ptMax);
+  }
+
+  f_flow = new TF1("f_flow",flowSample,-TMath::Pi(),TMath::Pi(),1);
+  f_v2   = readv2(energy,pid,cent);
+  f_spec = readspec(energy,pid,cent);
+  h_eta = readeta(energy,pid,cent);
+
+  // float const Resolution = 0.81; // xin's resolution
+  float const Resolution = readRes(energy,pid,cent);
+  cout << "InPut Resolution = " << Resolution << endl;
+  float const chi = getChi(Resolution);
+  f_EP = new TF1("f_EP",EventPlaneDist,-TMath::PiOver2(),TMath::PiOver2(),2);
+  f_EP->FixParameter(0,chi);
+  f_EP->FixParameter(1,0.5/TMath::Pi());
+
+  TStopwatch* stopWatch = new TStopwatch();
+  stopWatch->Start();
+  if(gRandom) delete gRandom;
+  gRandom = new TRandom3();
+  gRandom->SetSeed();
+
+  pydecay = TPythia6Decayer::Instance();
+  pydecay->Init();
+  setDecayChannels(pid); // phi--> K+K-
+
+  TClonesArray ptl("TParticle", 10);
+  TLorentzVector *lPhi = new TLorentzVector();
+  for(int i_ran = 0; i_ran < NMax; ++i_ran)
+  {
+    if (floor(10.0*i_ran/ static_cast<float>(NMax)) > floor(10.0*(i_ran-1)/ static_cast<float>(NMax)))
+    cout << "=> processing data: " << 100.0*i_ran/ static_cast<float>(NMax) << "%" << endl;
+
+    getKinematics(*lPhi,vmsa::InvMass[pid]);
+    decayAndFill(vmsa::decayMother[pid],lPhi,ptl);
+  }
+  cout << "=> processing data: 100%" << endl;
+  cout << "work done!" << endl;
+
+  write(energy);
+
+  stopWatch->Stop();   
+  stopWatch->Print();
+}
+
+float readRes(int energy, int pid, int centrality)
+{
+  string InPutRes = Form("/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu%s/SpinAlignment/Phi/MonteCarlo/Data/Resolution.root",vmsa::mBeamEnergy[energy].c_str());
+  TFile *File_Res = TFile::Open(InPutRes.c_str());
+  string HistName = Form("h_mRes_Centrality_%d_EtaGap_0_Phi_SysErrors_0",centrality);
+  TH1F *h_mRes = (TH1F*)File_Res->Get(HistName.c_str());
+  float resGaus = h_mRes->GetBinContent(1);
+  float resBW   = h_mRes->GetBinContent(2);
+  cout << "resGaus = " << resGaus << ", resBW = " << resBW << endl;
+
+  return 0.5*(resGaus+resBW);
+}
+
+TF1* readv2(int energy, int pid, int centrality)
+{
+  string InPutV2 = Form("/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu%s/SpinAlignment/Phi/MonteCarlo/Data/Phi_v2_1040.root",vmsa::mBeamEnergy[energy].c_str());
+  TFile *File_v2 = TFile::Open(InPutV2.c_str());
+  TGraphAsymmErrors *g_v2 = (TGraphAsymmErrors*)File_v2->Get("g_v2");
+  TF1 *f_v2 = new TF1("f_v2",v2_pT_FitFunc,vmsa::ptMin,vmsa::ptMax,5);
+  f_v2->FixParameter(0,2);
+  f_v2->SetParameter(1,0.1);
+  f_v2->SetParameter(2,0.1);
+  f_v2->SetParameter(3,0.1);
+  f_v2->SetParameter(4,0.1);
+  f_v2->SetLineColor(2);
+  f_v2->SetLineWidth(2);
+  f_v2->SetLineStyle(2);
+  g_v2->Fit(f_v2,"N");
+
+  /*
+  TCanvas *c_v2 = new TCanvas("c_v2","c_v2",10,10,800,800);
+  c_v2->cd()->SetLeftMargin(0.15);
+  c_v2->cd()->SetBottomMargin(0.15);
+  c_v2->cd()->SetTicks(1,1);
+  c_v2->cd()->SetGrid(0,0);
+  TH1F *h_v2 = new TH1F("h_v2","h_v2",100,0.0,10.0);
+  for(int i_bin = 1; i_bin < 101; ++i_bin)
+  {
+    h_v2->SetBinContent(i_bin,-10.0);
+    h_v2->SetBinError(i_bin,1.0);
+  }
+  h_v2->SetTitle("");
+  h_v2->SetStats(0);
+  h_v2->GetXaxis()->SetTitle("p_{T} (GeV/c)");
+  h_v2->GetXaxis()->CenterTitle();
+  h_v2->GetYaxis()->SetTitle("v_{2}");
+  h_v2->GetYaxis()->CenterTitle();
+  h_v2->GetYaxis()->SetRangeUser(0.0,0.2);
+  h_v2->Draw("pE");
+  g_v2->Draw("pE same");
+  f_v2->Draw("l same");
+  */
+
+  return f_v2;
+}
+
+TF1* readspec(int energy, int pid, int centrality)
+{
+  string InPutSpec = Form("/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu%s/SpinAlignment/Phi/MonteCarlo/Data/Phi_Spec.root",vmsa::mBeamEnergy[energy].c_str());
+  TFile *File_Spec = TFile::Open(InPutSpec.c_str());
+  TGraphAsymmErrors *g_spec = (TGraphAsymmErrors*)File_Spec->Get("g_spec");
+  TF1 *f_Levy = new TF1("f_Levy",Levy,vmsa::ptMin,vmsa::ptMax,3);
+  f_Levy->SetParameter(0,1);
+  f_Levy->SetParameter(1,10);
+  f_Levy->SetParameter(2,0.1);
+  f_Levy->SetLineStyle(2);
+  f_Levy->SetLineColor(4);
+  f_Levy->SetLineWidth(2);
+  g_spec->Fit(f_Levy,"N");
+
+  TF1 *f_spec = new TF1("f_spec",pTLevy,vmsa::ptMin,vmsa::ptMax,3);
+  f_spec->SetParameter(0,f_Levy->GetParameter(0));
+  f_spec->SetParameter(1,f_Levy->GetParameter(1));
+  f_spec->SetParameter(2,f_Levy->GetParameter(2));
+  f_spec->SetLineStyle(2);
+  f_spec->SetLineColor(2);
+  f_spec->SetLineWidth(2);
+
+  /*
+  TCanvas *c_spec = new TCanvas("c_spec","c_spec",10,10,800,800);
+  c_spec->cd()->SetLeftMargin(0.15);
+  c_spec->cd()->SetBottomMargin(0.15);
+  c_spec->cd()->SetTicks(1,1);
+  c_spec->cd()->SetGrid(0,0);
+  c_spec->SetLogy();
+  TH1F *h_spec = new TH1F("h_spec","h_spec",100,0.0,10.0);
+  for(int i_bin = 1; i_bin < 101; ++i_bin)
+  {
+    h_spec->SetBinContent(i_bin,-10.0);
+    h_spec->SetBinError(i_bin,1.0);
+  }
+  h_spec->SetTitle("");
+  h_spec->SetStats(0);
+  h_spec->GetXaxis()->SetTitle("p_{T} (GeV/c)");
+  h_spec->GetXaxis()->CenterTitle();
+  h_spec->GetYaxis()->SetTitle("dN/p_{T}dp_{T}");
+  h_spec->GetYaxis()->CenterTitle();
+  h_spec->GetYaxis()->SetRangeUser(1E-6,10);
+  h_spec->Draw("pE");
+  g_spec->Draw("pE same");
+  f_Levy->Draw("l same");
+  f_spec->Draw("l same");
+  */
+
+  return f_spec;
+}
+
+TH1F* readeta(int energy, int pid, int centrality)
+{
+  string InPutEta = Form("/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu%s/SpinAlignment/Phi/MonteCarlo/Data/Phi_Eta.root",vmsa::mBeamEnergy[energy].c_str());
+  TFile *File_Eta = TFile::Open(InPutEta.c_str());
+  TH2F *h_mEta_Cos_sig = (TH2F*)File_Eta->Get("h_mEta_Cos_sig");
+  TH1F *h_eta = (TH1F*)h_mEta_Cos_sig->ProjectionX();
+
+  return h_eta;
+}
+
+void getKinematics(TLorentzVector& lPhi, double const mass)
+{
+  double const pt = f_spec->GetRandom(vmsa::ptMin, vmsa::ptMax);
+  double const eta = h_eta->GetRandom();
+  f_flow->ReleaseParameter(0);
+  f_flow->SetParameter(0,f_v2->Eval(pt));
+  double const phi = f_flow->GetRandom();
+
+  // double const pt = gRandom->Uniform(vmsa::ptMin, vmsa::ptMax);
+  // double const y = gRandom->Uniform(-vmsa::acceptanceRapidity, vmsa::acceptanceRapidity);
+  // double const phi = TMath::TwoPi() * gRandom->Rndm();
+
+  lPhi.SetPtEtaPhiM(pt,eta,phi,mass);
+}
+
+void setDecayChannels(int const pid)
+{
+  int const mdme = vmsa::decayChannels[pid];
+  cout << "mdme = " << mdme << endl;
+  for (int idc = vmsa::decayChannelsFirst[pid]; idc < vmsa::decayChannelsSecond[pid] + 1; idc++) TPythia6::Instance()->SetMDME(idc, 1, 0); // close all decay channel
+  TPythia6::Instance()->SetMDME(mdme, 1, 1); // open the one we need
+  int *PYSeed = new int;
+  TPythia6::Instance()->SetMRPY(1,(int)PYSeed); // Random seed
+}
+
+void decayAndFill(int const kf, TLorentzVector* lPhi, TClonesArray& daughters)
+{
+  pydecay->Decay(kf, lPhi);
+  pydecay->ImportParticles(&daughters);
+
+  TLorentzVector lKplus;
+  TLorentzVector lKminus;
+
+  int nTrk = daughters.GetEntriesFast();
+  for (int iTrk = 0; iTrk < nTrk; ++iTrk)
+  {
+    TParticle* ptl0 = (TParticle*)daughters.At(iTrk);
+
+    switch (ptl0->GetPdgCode())
+    {
+      case 2212:
+	ptl0->Momentum(lKplus);
+	break;
+      case -211:
+	ptl0->Momentum(lKminus);
+	break;
+      default:
+	break;
+    }
+  }
+  daughters.Clear("C");
+
+  fill(lPhi,lKplus,lKminus);
+}
+
+void fill(TLorentzVector* lPhi, TLorentzVector const& lKplus, TLorentzVector const& lKminus)
+{
+  TVector3 vMcKpBoosted = CalBoostedVector(lKplus,lPhi); // boost Kplus back to phi-meson rest frame
+
+  TVector3 nQ(0.0,-1.0,0.0); // direction of angular momentum with un-smeared EP
+  float CosThetaStarRP = vMcKpBoosted.Dot(nQ);
+  p_cosRP->Fill(lPhi->Pt(),CosThetaStarRP);
+  h_Tracks->Fill(lPhi->Pt(),lPhi->Eta(),lPhi->Phi());
+
+  float Pt_lPhi = lPhi->Pt();
+  float Eta_lPhi = lPhi->Eta();
+  float Eta_lKplus = lKplus.Eta();
+  float Eta_lKminus = lKminus.Eta();
+
+  for(int i_eta = 0; i_eta < 20; ++i_eta)
+  {
+    if( passEtaCut(Eta_lPhi,i_eta) ) p_CosEtaPhi[i_eta]->Fill(Pt_lPhi,CosThetaStarRP);
+
+    if( passEtaCut(Eta_lKplus,i_eta) && passEtaCut(Eta_lKminus,i_eta) && passEtaCut(Eta_lPhi,i_eta) )
+      p_CosEtaKaon[i_eta]->Fill(Pt_lPhi,CosThetaStarRP);
+  }
+}
+
+float getChi(float Resolution)
+{
+  TF1 *f_res = new TF1("f_res",EventPlaneResolution,0,10,0);
+  double chi = f_res->GetX(Resolution);
+
+  return chi;
+}
+
+float EventPlaneSmearing(TF1 *f_EP)
+{
+  float Psi2 = f_EP->GetRandom(-TMath::PiOver2(),TMath::PiOver2());
+  return Psi2;
+}
+
+TVector3 CalBoostedVector(TLorentzVector const lMcDau, TLorentzVector *lMcVec)
+{
+  TVector3 vMcBeta = -1.0*lMcVec->BoostVector(); // boost vector
+
+  TLorentzVector lKaon = lMcDau;
+  lKaon.Boost(vMcBeta); // boost Kplus back to phi-meson rest frame
+  TVector3 vMcDauStar = lKaon.Vect().Unit(); // momentum direction of Kplus in phi-meson rest frame
+
+  return vMcDauStar;
+}
+
+double calDipAngle(TLorentzVector const& lKplus, TLorentzVector const& lKminus)
+{
+  double KplusPt = lKplus.Pt();
+  double KplusPz = lKplus.Pz();
+  double KplusP  = lKplus.P();
+
+  double KminusPt = lKminus.Pt(); 
+  double KminusPz = lKminus.Pz(); 
+  double KminusP  = lKminus.P();
+
+  double costheta = (KplusPt*KminusPt+KplusPz*KminusPz)/(KplusP*KminusP);
+  double theta = acos(costheta);
+
+  return theta;
+}
+
+bool passDipAngleCut(TLorentzVector const& lKplus, TLorentzVector const& lKminus)
+{
+  double KplusPt = lKplus.Pt();
+  double KplusPz = lKplus.Pz();
+  double KplusP  = lKplus.P();
+
+  double KminusPt = lKminus.Pt(); 
+  double KminusPz = lKminus.Pz(); 
+  double KminusP  = lKminus.P();
+
+  double costheta = (KplusPt*KminusPt+KplusPz*KminusPz)/(KplusP*KminusP);
+  double theta = acos(costheta);
+  if(theta < 0.04) return kFALSE;
+
+  return kTRUE;
+}
+
+bool passEtaCut(float eta, int BinEta)
+{
+  if(TMath::Abs(eta) >= vmsa::McEtaBin[BinEta]) return kFALSE;
+
+  return kTRUE;
+}
+
+void write(int energy)
+{
+  string OutPutFile = Form("/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu%s/SpinAlignment/Phi/MonteCarlo/McLambdaEta.root",vmsa::mBeamEnergy[energy].c_str());
+  TFile *File_OutPut = new TFile(OutPutFile.c_str(),"RECREATE");
+  File_OutPut->cd();
+
+  h_Tracks->Write();
+  p_cosRP->Write();
+
+  for(int i_eta = 0; i_eta < 20; ++i_eta)
+  {
+    p_CosEtaKaon[i_eta]->Write();
+    p_CosEtaPhi[i_eta]->Write();
+  }
+
+  File_OutPut->Close();
+}
+
---
+++ McPhiResCorr/plotMcPhiEta.C
@@ -0,0 +1,196 @@
+#include <string>
+#include "TFile.h"
+#include "TF1.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TCanvas.h"
+#include "TGraphAsymmErrors.h"
+#include "TLine.h"
+#include "TLatex.h"
+#include "TGaxis.h"
+#include "TLegend.h"
+#include "TRandom3.h"
+#include "TStyle.h"
+#include "../Utility/functions.h"
+#include "../Utility/draw.h"
+#include "../Utility/StSpinAlignmentCons.h"
+
+void plotMcPhiEta()
+{
+  string InPutHist = "/Users/xusun/Data/SpinAlignment/AuAu200GeV/McPhiEta.root";
+  TFile *File_InPut = TFile::Open(InPutHist.c_str());
+
+  TH2F *h_cosRP = (TH2F*)File_InPut->Get("h_cosRP");
+  TH1F *h_Cos = (TH1F*)h_cosRP->ProjectionY();
+  TF1 *f_rho = new TF1("f_rho",SpinDensity,-1.0,1.0,2);
+  f_rho->SetParameter(0,0.33);
+  f_rho->SetParameter(1,100);
+  h_Cos->Fit(f_rho,"NQ");
+  float rho = f_rho->GetParameter(0);
+  float err = f_rho->GetParError(0);
+  float Norm = f_rho->GetParameter(1);
+
+  TH2F *h_CosEtaKaon[20], *h_CosEtaPhi[20];
+  TH1F *h_CosKaon[20], *h_CosPhi[20];
+  float Norm_Kaon[20], Norm_Phi[20];
+  float rho_Kaon[20], rho_Phi[20];
+  float err_Kaon[20], err_Phi[20];
+  TGraphAsymmErrors *g_Kaon = new TGraphAsymmErrors();
+  TGraphAsymmErrors *g_Phi = new TGraphAsymmErrors();
+  for(int i_eta = 0; i_eta < 20; ++i_eta)
+  {
+    string HistName;
+    HistName = Form("h_CosEtaKaon_%d",i_eta);
+    h_CosEtaKaon[i_eta] = (TH2F*)File_InPut->Get(HistName.c_str());
+    HistName = Form("h_CosKaon_%d",i_eta);
+    h_CosKaon[i_eta] = (TH1F*)h_CosEtaKaon[i_eta]->ProjectionY(HistName.c_str());
+    h_CosKaon[i_eta]->SetTitle("");
+    h_CosKaon[i_eta]->SetStats(0);
+    h_CosKaon[i_eta]->GetXaxis()->SetTitle("cos(#theta*)");
+    h_CosKaon[i_eta]->GetXaxis()->CenterTitle();
+    h_CosKaon[i_eta]->GetXaxis()->SetLabelSize(0.04);
+    h_CosKaon[i_eta]->GetXaxis()->SetNdivisions(505);
+
+    h_CosKaon[i_eta]->GetYaxis()->SetTitle("Counts");
+    h_CosKaon[i_eta]->GetYaxis()->SetTitleSize(0.04);
+    h_CosKaon[i_eta]->GetYaxis()->CenterTitle();
+    h_CosKaon[i_eta]->GetYaxis()->SetLabelSize(0.04);
+    h_CosKaon[i_eta]->GetYaxis()->SetNdivisions(505);
+    h_CosKaon[i_eta]->GetYaxis()->SetRangeUser(0.9*h_CosKaon[i_eta]->GetMinimum(),1.1*h_CosKaon[i_eta]->GetMaximum());
+
+    h_CosKaon[i_eta]->SetMarkerStyle(24);
+    h_CosKaon[i_eta]->SetMarkerSize(1.1);
+    h_CosKaon[i_eta]->SetMarkerColor(2);
+    h_CosKaon[i_eta]->SetLineColor(2);
+    TF1 *f_rhoKaon = new TF1("f_rhoKaon",SpinDensity,-1.0,1.0,2);
+    f_rhoKaon->SetParameter(0,0.33);
+    f_rhoKaon->SetParameter(1,100);
+    h_CosKaon[i_eta]->Fit(f_rhoKaon,"NQ");
+    rho_Kaon[i_eta] = f_rhoKaon->GetParameter(0);
+    err_Kaon[i_eta] = f_rhoKaon->GetParError(0);
+    Norm_Kaon[i_eta] = f_rhoKaon->GetParameter(1);
+    g_Kaon->SetPoint(i_eta,vmsa::McEtaBin[i_eta],rho_Kaon[i_eta]);
+    g_Kaon->SetPointError(i_eta,0.0,0.0,err_Kaon[i_eta],err_Kaon[i_eta]);
+
+    HistName = Form("h_CosEtaPhi_%d",i_eta);
+    h_CosEtaPhi[i_eta] = (TH2F*)File_InPut->Get(HistName.c_str());
+    HistName = Form("h_CosPhi_%d",i_eta);
+    h_CosPhi[i_eta] = (TH1F*)h_CosEtaPhi[i_eta]->ProjectionY(HistName.c_str());
+    h_CosPhi[i_eta]->SetTitle("");
+    h_CosPhi[i_eta]->SetStats(0);
+    h_CosPhi[i_eta]->GetXaxis()->SetTitle("cos(#theta*)");
+    h_CosPhi[i_eta]->GetXaxis()->CenterTitle();
+    h_CosPhi[i_eta]->GetXaxis()->SetLabelSize(0.04);
+    h_CosPhi[i_eta]->GetXaxis()->SetNdivisions(505);
+
+    h_CosPhi[i_eta]->GetYaxis()->SetTitle("Counts");
+    h_CosPhi[i_eta]->GetYaxis()->SetTitleSize(0.04);
+    h_CosPhi[i_eta]->GetYaxis()->CenterTitle();
+    h_CosPhi[i_eta]->GetYaxis()->SetLabelSize(0.04);
+    h_CosPhi[i_eta]->GetYaxis()->SetNdivisions(505);
+    h_CosPhi[i_eta]->GetYaxis()->SetRangeUser(0.9*h_CosPhi[i_eta]->GetMinimum(),1.1*h_CosPhi[i_eta]->GetMaximum());
+
+    h_CosPhi[i_eta]->SetMarkerStyle(24);
+    h_CosPhi[i_eta]->SetMarkerSize(1.1);
+    h_CosPhi[i_eta]->SetMarkerColor(4);
+    h_CosPhi[i_eta]->SetLineColor(4);
+    TF1 *f_rhoPhi = new TF1("f_rhoPhi",SpinDensity,-1.0,1.0,2);
+    f_rhoPhi->SetParameter(0,0.33);
+    f_rhoPhi->SetParameter(1,100);
+    h_CosPhi[i_eta]->Fit(f_rhoPhi,"NQ");
+    rho_Phi[i_eta] = f_rhoPhi->GetParameter(0);
+    err_Phi[i_eta] = f_rhoPhi->GetParError(0);
+    Norm_Phi[i_eta] = f_rhoPhi->GetParameter(1);
+    g_Phi->SetPoint(i_eta,vmsa::McEtaBin[i_eta],rho_Phi[i_eta]);
+    g_Phi->SetPointError(i_eta,0.0,0.0,err_Phi[i_eta],err_Phi[i_eta]);
+  }
+  g_Kaon->SetPoint(20,8.0,rho);
+  g_Kaon->SetPointError(20,0.0,0.0,err,err);
+  g_Phi->SetPoint(20,8.0,rho);
+  g_Phi->SetPointError(20,0.0,0.0,err,err);
+
+  TCanvas *c_EtaCutEff = new TCanvas("c_EtaCutEff","c_EtaCutEff",10,10,1600,800);
+  c_EtaCutEff->Divide(2,1);
+  for(int i_pad = 0; i_pad < 2; ++i_pad)
+  {
+    c_EtaCutEff->cd(i_pad+1)->SetLeftMargin(0.15);
+    c_EtaCutEff->cd(i_pad+1)->SetBottomMargin(0.15);
+    c_EtaCutEff->cd(i_pad+1)->SetTicks(1,1);
+    c_EtaCutEff->cd(i_pad+1)->SetGrid(0,0);
+  }
+  c_EtaCutEff->cd(1);
+  h_CosKaon[10]->Draw("pE");
+  TF1 *f_rhoKaonQA = new TF1("f_rhoKaonQA",SpinDensity,-1.0,1.0,2);
+  f_rhoKaonQA->SetParameter(0,rho_Kaon[10]);
+  f_rhoKaonQA->SetParameter(1,Norm_Kaon[10]);
+  f_rhoKaonQA->SetLineColor(2);
+  f_rhoKaonQA->SetLineWidth(2);
+  f_rhoKaonQA->SetLineStyle(1);
+  f_rhoKaonQA->Draw("l same");
+  plotTopLegend("cut on K and #phi-meson",0.2,0.80,0.04,1,0.0,42,1);
+  plotTopLegend("|#eta| < 1.0",0.2,0.75,0.04,1,0.0,42,1);
+  string printRhoKaon = Form("#rho_{00} = %1.3f #pm %1.3f",rho_Kaon[10],err_Kaon[10]);
+  plotTopLegend((char*)printRhoKaon.c_str(),0.2,0.70,0.04,1,0.0,42,1);
+
+  c_EtaCutEff->cd(2);
+  h_CosPhi[10]->Draw("pE");
+  TF1 *f_rhoPhiQA = new TF1("f_rhoPhiQA",SpinDensity,-1.0,1.0,2);
+  f_rhoPhiQA->SetParameter(0,rho_Phi[10]);
+  f_rhoPhiQA->SetParameter(1,Norm_Phi[10]);
+  f_rhoPhiQA->SetLineColor(4);
+  f_rhoPhiQA->SetLineWidth(2);
+  f_rhoPhiQA->SetLineStyle(1);
+  f_rhoPhiQA->Draw("l same");
+  plotTopLegend("cut on #phi-meson only",0.2,0.80,0.04,1,0.0,42,1);
+  plotTopLegend("|#eta| < 1.0",0.2,0.75,0.04,1,0.0,42,1);
+  string printRhoPhi = Form("#rho_{00} = %1.3f #pm %1.3f",rho_Phi[10],err_Phi[10]);
+  plotTopLegend((char*)printRhoPhi.c_str(),0.2,0.70,0.04,1,0.0,42,1);
+  c_EtaCutEff->SaveAs("../figures/c_phiEtaCutEff.eps");
+
+
+
+  TCanvas *c_rhoEta = new TCanvas("c_rhoEta","c_rhoEta",10,10,800,800);
+  c_rhoEta->cd()->SetLeftMargin(0.15);
+  c_rhoEta->cd()->SetBottomMargin(0.15);
+  c_rhoEta->cd()->SetTicks(1,1);
+  c_rhoEta->cd()->SetGrid(0,0);
+
+  TH1F *h_rho = new TH1F("h_rho","h_rho",100,0.0,10.0);
+  for(int i_bin = 1; i_bin < 101; ++i_bin)
+  {
+    h_rho->SetBinContent(i_bin,-10.0);
+    h_rho->SetBinError(i_bin,1.0);
+  }
+  h_rho->SetTitle("");
+  h_rho->SetStats(0);
+  h_rho->GetXaxis()->SetTitle("|#eta| <");
+  h_rho->GetXaxis()->CenterTitle();
+  h_rho->GetXaxis()->SetRangeUser(0.0,10.0);
+  h_rho->GetYaxis()->SetTitle("#rho_{00}");
+  h_rho->GetYaxis()->SetTitleSize(0.04);
+  h_rho->GetXaxis()->SetNdivisions(505);
+
+  h_rho->GetYaxis()->CenterTitle();
+  h_rho->GetYaxis()->SetRangeUser(0.25,0.6);
+  h_rho->GetYaxis()->SetNdivisions(505);
+  h_rho->Draw("pE");
+  PlotLine(0.0,10.0,1.0/3.0,1.0/3.0,1,2,2);
+
+  g_Kaon->SetMarkerStyle(24);
+  g_Kaon->SetMarkerColor(2);
+  g_Kaon->SetMarkerSize(1.4);
+  g_Kaon->Draw("pE same");
+
+  g_Phi->SetMarkerStyle(24);
+  g_Phi->SetMarkerColor(4);
+  g_Phi->SetMarkerSize(1.4);
+  g_Phi->Draw("pE same");
+
+  TLegend *leg = new TLegend(0.4,0.6,0.8,0.8);
+  leg->SetBorderSize(0);
+  leg->SetFillColor(0);
+  leg->AddEntry(g_Kaon,"cut on K and #phi-meson","p");
+  leg->AddEntry(g_Phi,"cut on #phi-meson only","p");
+  leg->Draw("same");
+  c_rhoEta->SaveAs("../figures/c_phiRhoEta.eps");
+}
