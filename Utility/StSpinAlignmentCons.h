abd8d889cbab88a74cdb8ad67ca0a6526e990bf5
Xu Sun <sunxuhit@gmail.com>
2017-06-15 11:30:56 +0800

    v1.54: removed Lambda-related simulation

--- Utility/StSpinAlignmentCons.h
+++ Utility/StSpinAlignmentCons.h
@@ -189,18 +189,11 @@ namespace vmsa
   // used in McPhiResCorr
   double const acceptanceRapidity = 1.0;
   float const rhoDelta = 0.01;
-  // float const InvMass[3] = {1.01946,0.89594,0.49761}; // 0: phi, 1: K*, 2 K0S
-  // int const decayChannelsFirst[3] = {656,617,613};
-  // int const decayChannelsSecond[3] = {666,619,614};
-  // int const decayMother[3] = {333,313,310};
-  // int const decayChannels[3] = {656,617,613}; // 0: phi->K+K-, 1: K*->Kpi, 2 K0S->pi+pi-
-
-  float const InvMass[3] = {1.01946,1.11568,0.49761}; // 0: phi, 1: Lambda, 2 K0S
-  int const decayChannelsFirst[3] = {656,1058,613};
-  int const decayChannelsSecond[3] = {666,1061,614};
-  int const decayMother[3] = {333,3122,310};
-  int const decayChannels[3] = {656,1058,613}; // 0: phi->K+K-, 1: Lambda->ppi, 2 K0S->pi+pi-
-
+  float const InvMass[3] = {1.01946,0.89594,0.49761}; // 0: phi, 1: K*, 2 K0S
+  int const decayChannelsFirst[3] = {656,617,613};
+  int const decayChannelsSecond[3] = {666,619,614};
+  int const decayMother[3] = {333,313,310};
+  int const decayChannels[3] = {656,617,613}; // 0: phi->K+K-, 1: K*->Kpi, 2 K0S->pi+pi-
   float const McEtaBin[20] = {0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5,2.0,2.5,3.0,3.5,4.0};
 
   // used in RcPhiEffCorr
