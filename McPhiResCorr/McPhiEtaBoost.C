e8a3095d79cd1a37785c85b3d0e79eaece01364a
Xu Sun <sunxuhit@gmail.com>
2017-08-08 16:02:01 +0800

    v1.964: testing

--- McPhiResCorr/McPhiEtaBoost.C
+++ McPhiResCorr/McPhiEtaBoost.C
@@ -223,7 +223,7 @@ void getKinematics(TLorentzVector& lPhi, double const mass)
   // double const eta = gRandom->Uniform(-6.0*vmsa::acceptanceRapidity, 6.0*vmsa::acceptanceRapidity);
   // double const phi = TMath::TwoPi() * gRandom->Rndm();
 
-  lPhi.SetXYZM(1.0,1.0,1.0,mass);
+  lPhi.SetXYZM(0.0,0.0,0.0,mass);
 }
 
 void setDecayChannels(int const pid)
