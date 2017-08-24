ac60a453092fbecfb2350f30001a515cf225d0f7
Xu Sun <sunxuhit@gmail.com>
2017-08-24 11:46:23 -0400

    v1.992: update shell script for Lambda MC simulation

--- McPhiResCorr/submit/McLambdaEta.sh
+++ McPhiResCorr/submit/McLambdaEta.sh
@@ -8,7 +8,7 @@ then
   PID="Lambda"
   Energy="200GeV"
   Name="Mc"$PID$Energy
-  for((counter=0;counter<10;counter=counter+1))
+  for((counter=0;counter<100;counter=counter+1))
   do
     cp ./run.csh ./run$Name${counter}.csh
 
@@ -19,7 +19,7 @@ then
     echo "root4star -b -q -x McLambdaEta.C++'('6','0','0','5000000','$counter')'" >> ./run$Name${counter}.csh
     echo "cd .." >> ./run$Name${counter}.csh
     echo "rm -rf $Name$counter" >> ./run$Name${counter}.csh
-    qsub -hard -l scratchfree=500,h_cpu=4:00:00,h_vmem=1.8G,projectio=1 -o /global/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu$Energy/Log/job$Name$counter.log -e /global/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu$Energy/Log/job$Name$counter.err ./run$Name$counter.csh
+    qsub -hard -l scratchfree=500,h_cpu=2:00:00,h_vmem=1.8G,projectio=1 -o /global/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu$Energy/Log/job$Name$counter.log -e /global/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu$Energy/Log/job$Name$counter.err ./run$Name$counter.csh
     mv ./run$Name${counter}.csh /global/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu$Energy/Script/
   done
 else
