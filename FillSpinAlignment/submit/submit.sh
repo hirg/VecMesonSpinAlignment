2a80ad65911b1c708759192169813e6286e1d5b4
Xu Sun <sunxuhit@gmail.com>
2017-11-20 14:17:27 -0800

    v0.25_SysErrors: modified submit group

--- FillSpinAlignment/submit/submit.sh
+++ FillSpinAlignment/submit/submit.sh
@@ -5,8 +5,8 @@ date
 
 if [ $# -eq 0 ]
 then
-  # sbatch --account star --ntasks=1 --array=0-9 --time=01:00:00 FillSpinAlignment.slr # test mode
-  sbatch --account star --ntasks=1 --array=0-32 --time=08:00:00 FillSpinAlignment.slr # production mode
+  # sbatch --account rhstar --ntasks=1 --array=0-9 --time=01:00:00 FillSpinAlignment.slr # test mode
+  sbatch --account rhstar --ntasks=1 --array=0-32 --time=08:00:00 FillSpinAlignment.slr # production mode
 else
   echo "Wrong number of parameters!!"
 fi
--- TreeProduction/submit/submit.sh
+++ TreeProduction/submit/submit.sh
@@ -5,8 +5,8 @@ date
 
 if [ $# -eq 0 ]
 then
-  sbatch --account star --ntasks=1 --array=0-19 --time=8:00:00 VecMesonTree.slr # phi TTree mode
-  # sbatch --account star --ntasks=1 --array=10000-10019 --time=8:00:00 VecMesonTree.slr # phi TTree resubmit mode
+  sbatch --account rhstar --ntasks=1 --array=0-19 --time=8:00:00 VecMesonTree.slr # phi TTree mode
+  # sbatch --account rhstar --ntasks=1 --array=10000-10019 --time=8:00:00 VecMesonTree.slr # phi TTree resubmit mode
 else
   echo "Wrong number of parameters!!"
 fi
